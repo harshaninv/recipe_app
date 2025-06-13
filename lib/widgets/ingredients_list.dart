307f2a5cf1d4066e038a88a82297cfd45a3e04b3
Harsh0726 <harshanineranjana@gmail.com>
2025-06-13 16:56:02 +0530

    Created Ingredients list tiles

---
+++ lib/widgets/ingredients_list.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/theme/app_text_theme.dart';
+import 'package:recipe_app/theme/color_theme.dart';
+
+class IngredientsList extends StatefulWidget {
+  const IngredientsList({super.key, required this.ingredients});
+
+  final List<String> ingredients;
+
+  @override
+  State<IngredientsList> createState() => _IngredientsListState();
+}
+
+class _IngredientsListState extends State<IngredientsList> {
+  late List<bool> _checkedIngredients;
+
+  @override
+  void initState() {
+    _checkedIngredients = List<bool>.filled(widget.ingredients.length, false);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: widget.ingredients.length,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        // return Text(ingredients[index]);
+        return CheckboxListTile(
+          value: _checkedIngredients[index],
+          onChanged: (value) {
+            setState(() {
+              _checkedIngredients[index] = value!;
+            });
+          },
+          title: Text(widget.ingredients[index], style: AppTextTheme.textTheme.headlineLarge!.copyWith(
+            color: const Color.fromARGB(255, 49, 11, 28),
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),),
+          activeColor:  const Color.fromARGB(255, 49, 11, 28),
+        );
+      },
+    );
+  }
+}
---
+++ lib/widgets/instroctions_list.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/theme/app_text_theme.dart';
+import 'package:recipe_app/theme/color_theme.dart';
+
+class InstroctionsList extends StatefulWidget {
+  const InstroctionsList({super.key, required this.instructions});
+
+  final List<String> instructions;
+
+  @override
+  State<InstroctionsList> createState() => _InstroctionsListState();
+}
+
+class _InstroctionsListState extends State<InstroctionsList> {
+  late List<bool> _checkedInstructions;
+
+  @override
+  void initState() {
+    _checkedInstructions = List<bool>.filled(widget.instructions.length, false);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: widget.instructions.length,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        // return Text(ingredients[index]);
+        return CheckboxListTile(
+          value: _checkedInstructions[index],
+          onChanged: (value) {
+            setState(() {
+              _checkedInstructions[index] = value!;
+            });
+          },
+          title: Text('${index+1}). ${widget.instructions[index]}', style: AppTextTheme.textTheme.headlineMedium!.copyWith(
+            fontSize: 20,
+            fontWeight: FontWeight.normal
+          ),),
+        );
+      },
+    );
+  }
+}
