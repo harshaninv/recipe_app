522ce3b68acb4046e2f4eb6940c48ded9b28ed8e
Harsh0726 <harshanineranjana@gmail.com>
2025-07-23 13:28:42 +0530

    chaned to mutltiprovider

--- lib/screens/recipe_app.dart
+++ lib/screens/recipe_app.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:recipe_app/main.dart';
+import 'package:recipe_app/providers/auth_provider.dart';
 import 'package:recipe_app/providers/recipe_provider.dart';
+import 'package:recipe_app/screens/auth/login_screen.dart';
+import 'package:recipe_app/screens/auth/signup_screen.dart';
 import 'package:recipe_app/screens/home_screen.dart';
 
 class RecipeApp extends StatelessWidget {
@@ -9,13 +12,31 @@ class RecipeApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => RecipeProvider(),
+    // return ChangeNotifierProvider(
+    //   create: (_) => RecipeProvider(),
+    //   child: MaterialApp(
+    //     title: 'Recipe Book',
+    //     debugShowCheckedModeBanner: false,
+    //     theme: darkTheme,
+    //     home: const HomeScreen(),
+    //   ),
+    // );
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => RecipeProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
       child: MaterialApp(
         title: 'Recipe Book',
         debugShowCheckedModeBanner: false,
         theme: darkTheme,
-        home: const HomeScreen(),
+        home: const LoginScreen(),
+        initialRoute: '/login',
+        routes: {
+          '/login': (context) => LoginScreen(),
+          '/signup': (context) => SignupScreen(),
+          '/home': (context) => HomeScreen(),
+        },
       ),
     );
   }
