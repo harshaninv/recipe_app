f90e08e23909106d7544b088493be43a51b7f19c
Harsh0726 <harshanineranjana@gmail.com>
2025-07-26 10:59:07 +0530

    added drawer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:recipe_app/models/recipe.dart';
+import 'package:recipe_app/providers/auth_provider.dart';
 import 'package:recipe_app/providers/recipe_provider.dart';
 import 'package:recipe_app/screens/recipe_detial.dart';
+import 'package:recipe_app/widgets/app_drawer.dart';
 import 'package:recipe_app/widgets/custom_scaffold.dart';
 import 'package:recipe_app/widgets/recipe_item_card.dart';
 
@@ -42,36 +44,45 @@ class _HomeScreenState extends State<HomeScreen> {
     final recipeProvider = Provider.of<RecipeProvider>(context);
     final List<Recipe> listOfRecipes = recipeProvider.recipes;
 
-    return Scaffold(
+    return CustomScaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: const Text('Food Recipes'), 
+        title: const Text('Food Recipes'),
         centerTitle: true,
         titleTextStyle: Theme.of(context).textTheme.headlineLarge,
+        actions: [
+          Builder(
+            builder: (ctx) => IconButton(
+              onPressed: () {
+                Scaffold.of(ctx).openDrawer();
+              },
+              icon: Icon(Icons.menu),
+            ),
+          ),
+        ],
       ),
-      body: CustomScaffold(
-        child: recipeProvider.isLoading
-            ? const Align(
+      drawer: const Drawer(child: RecipeAppDrawer()),
+      child: recipeProvider.isLoading
+          ? const Align(
               alignment: Alignment.topCenter,
               child: LinearProgressIndicator(),
             )
-            : Padding(
-                padding: const EdgeInsets.only(top: 4.0),
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                  ),
-                  itemCount: listOfRecipes.length,
-                  itemBuilder: (context, index) {
-                    return RecipeItemCard(
-                      recipe: listOfRecipes[index],
-                      onTapRecipe: () =>
-                          _selectMeal(context, listOfRecipes[index]),
-                    );
-                  },
+          : Padding(
+              padding: const EdgeInsets.only(top: 4.0),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
                 ),
+                itemCount: listOfRecipes.length,
+                itemBuilder: (context, index) {
+                  return RecipeItemCard(
+                    recipe: listOfRecipes[index],
+                    onTapRecipe: () =>
+                        _selectMeal(context, listOfRecipes[index]),
+                  );
+                },
               ),
-      ),
+            ),
     );
   }
 }
