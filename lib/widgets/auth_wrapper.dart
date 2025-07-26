543b5676763fbfa1585f032ff73245b5b0f184cb
Harsh0726 <harshanineranjana@gmail.com>
2025-07-26 11:01:53 +0530

    creted Authwrapper for check tha user login accessbility

---
+++ lib/widgets/auth_wrapper.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:recipe_app/providers/auth_provider.dart';
+import 'package:recipe_app/screens/auth/login_screen.dart';
+import 'package:recipe_app/screens/home_screen.dart';
+import 'package:recipe_app/widgets/custom_scaffold.dart';
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+
+    switch (authProvider.status) {
+      case AuthStatus.loading:
+        return const CustomScaffold(
+          child: Center(child: CircularProgressIndicator()),
+        );
+      case AuthStatus.authenticated:
+        return const HomeScreen();
+      case AuthStatus.unauthenticated:
+        return const LoginScreen();
+    }
+  }
+}
