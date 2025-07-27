3794603b60504b4513452330afc92ef21fdc31b1
Harsh0726 <harshanineranjana@gmail.com>
2025-07-27 14:34:59 +0530

    removed unwanted code

--- lib/widgets/custom_scaffold.dart
+++ lib/widgets/custom_scaffold.dart
@@ -1,19 +1,18 @@
+
 import 'package:flutter/material.dart';
 
 class CustomScaffold extends StatelessWidget {
-  const CustomScaffold({super.key, required this.child, this.appBar, this.drawer, this.bottomNavigateionBar});
+  const CustomScaffold({super.key, required this.child, this.appBar, this.drawer});
 
   final Widget child;
   final AppBar? appBar;
   final Drawer? drawer;
-  final BottomAppBar? bottomNavigateionBar;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar,
       drawer: drawer,
-      bottomNavigationBar: bottomNavigateionBar,
       body: Stack(
         children: [
           Image.asset(
